bfbdc2ece7e4d6e3d10fa5acf68b117b8f496148
wangpingfei <1349304594@qq.com>
2017-10-15 23:07:20 +0800

    初始化提交

---
+++ src/main/java/com/wpf/qlexpress/test/MyTest.java
@@ -0,0 +1,24 @@
+package com.wpf.qlexpress.test;
+
+import com.ql.util.express.DefaultContext;
+import com.ql.util.express.ExpressRunner;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		ExpressRunner runner = new ExpressRunner();
+		DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+		context.put("a",1);
+        context.put("b",2);
+        context.put("c",3);
+        String express = "a+b*c";
+        try {
+			Object r = runner.execute(express, context, null, true, false);
+			System.out.println(r);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
